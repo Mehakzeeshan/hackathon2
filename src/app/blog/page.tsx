0143aec524e674893e76a5c2059da7e786f121db
Mehak Noor <mehaknoor1001@gmail.com>
2024-12-16 13:22:14 -0800

    new pages added

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar";
+import Footer from "../components/footer";
+import Review from "../components/review";
+import BlogSection from "../components/blogSection";
+
+export default function Blog () {
+    return (
+        <>
+        <Navbar />
+        <BlogSection />
+        <Review />
+        <Footer />
+        </>
+    )
+}
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/navbar";
+import Review from "../components/review";
+import Footer from "../components/footer";
+import ContactSection from "../components/contactSection";
+
+export default function Contact () {
+    return (
+       <>
+       <Navbar />
+       <ContactSection />
+       <Review />
+       <Footer />
+       </>
+    )
+}
